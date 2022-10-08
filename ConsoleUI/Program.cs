ee0457f896944a1cb5aa38d95b65a539149113ae
baran <byasarn@gmail.com>
2022-10-08 09:18:51 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+InMemoryProductDal productDal = new InMemoryProductDal();
+foreach (var item in productManager.GetAll())
+{
+    Console.WriteLine(item.ProductName);
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,71 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>() { 
+            new Product{CategoryID=1,ProductID=1,ProductName="bardak",
+            UnitPrice=15,UnitInStock=15},
+            new Product{CategoryID=1,ProductID=2,ProductName="kamera",
+            UnitPrice=500,UnitInStock=3},
+                new Product{CategoryID=2,ProductID=3,ProductName="telefon",
+            UnitPrice=1500,UnitInStock=2},
+                new Product{CategoryID=2,ProductID=4,ProductName="klavye",
+            UnitPrice=150,UnitInStock=65},
+                new Product{CategoryID=2,ProductID=5,ProductName="fare",
+            UnitPrice=85,UnitInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+        _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            _products.Remove(productToDelete);
+
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductID == p.ProductID)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+            //_products.Remove(productToDelete);
+
+
+        }
+
+        public List<Product> GetAll()
+        {
+        return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryID)
+        {
+           return _products.Where(p => p.CategoryID == categoryID).ToList();   
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.CategoryID = product.CategoryID;
+
+        }
+    }
+}
