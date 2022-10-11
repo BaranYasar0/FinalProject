b7daad7a936b0b168edd563152a0849b4cb79f5a
baran <byasarn@gmail.com>
2022-10-11 15:40:16 +0300

    Kamp 12 sonu

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolver.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -11,8 +14,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(options =>
+//    options.RegisterModule(new AutofacBusinessModule())));
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(options =>
+{
+    options.RegisterModule(new AutofacBusinessModule());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
